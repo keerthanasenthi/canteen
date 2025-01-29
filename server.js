3534efe88f409ff76973c639bf0c2e74d43881ad
Keerthana <keerthana.s2023cce@sece.ac.in>
2025-01-29 11:59:40 +0530

    code changed

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const http = require('http');
 const express = require('express');
 const app = express();
@@ -11,23 +10,4 @@ const server = http.createServer(app);
 
 server.listen(3000, () => {
   console.log('Server is running on port 3000');
-=======
-const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-const app = express();
-const PORT = process.env.PORT || 3000; // Use Render's assigned port
-
-app.use(express.json());
-
-// Sample route
-app.get("/", (req, res) => {
-  res.send("Server is running!");
-});
-
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
->>>>>>> 54b69bc017fed75d829997dafca55f00872c1fdf
 });
